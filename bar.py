41d8291654e44678c41c5d660a34965fceb0514e
Mengjun74 <91912728+Mengjun74@users.noreply.github.com>
2022-11-23 14:34:40 -0800

    changed name

--- bar.py
+++ bar.py
@@ -1,6 +1,6 @@
 "This is a comment for the python file bar that changes the string SAM to its corresponding ascii characters"
 
-name = "SAM"
+name = "jun"
 bar = 0
 for i in name:
     bar += ord(i)
